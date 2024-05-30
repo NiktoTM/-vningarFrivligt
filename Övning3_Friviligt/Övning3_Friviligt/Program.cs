3611aeddbecbfa4a7cc74325ba407e90b45ba6b5
NiktoTM <112383619+NiktoTM@users.noreply.github.com>
2024-05-30 13:02:46 +0200

    Add files via upload

---
+++ Övning3_Friviligt/Övning3_Friviligt/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+interface IFirstInterface
+{
+    void myMethod(); // Interface method
+}
+
+interface ISecondInterface
+{
+    void myOtherMethod(); // Interface method
+}
+
+// New interface
+interface IThirdInterface
+{
+    void myBestMethod(); // Abstract method
+}
+
+// Multiple interfaces
+class DemoClass : IFirstInterface, ISecondInterface, IThirdInterface
+{
+    public void myMethod()
+    {
+        Console.WriteLine("Some text..");
+    }
+
+    public void myOtherMethod()
+    {
+        Console.WriteLine("Some other text...");
+    }
+
+    public void myBestMethod()
+    {
+        Console.WriteLine("My best text...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DemoClass myObj = new DemoClass();
+        myObj.myMethod();
+        myObj.myOtherMethod();
+        myObj.myBestMethod(); // Call the new method
+    }
+}
