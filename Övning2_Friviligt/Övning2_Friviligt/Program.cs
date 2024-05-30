6c0b7573dc283c148708aefc423783ad1168cd5d
NiktoTM <112383619+NiktoTM@users.noreply.github.com>
2024-05-30 13:01:27 +0200

    Add files via upload

---
+++ Övning2_Friviligt/Övning2_Friviligt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Interface
+interface IAnimal
+{
+    void animalSound(); 
+}
+
+class Pig : IAnimal
+{
+    public void animalSound()
+    {
+        
+        Console.WriteLine("The pig says: wee wee");
+    }
+}
+
+class Dog : IAnimal
+{
+    public void animalSound()
+    {
+        
+        Console.WriteLine("The dog says: voff");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pig myPig = new Pig();  // Create a Pig object
+        myPig.animalSound();
+
+        Dog myDog = new Dog();  // Create a Dog object
+        myDog.animalSound();
+    }
+}
